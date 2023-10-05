2b1ff7120cc13d503984a0ee5f8969e156925e58
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-10-05 14:43:48 +0800

    resolve the path error for username

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
-import UserName from '../features/user/UserName';
+import UserName from '../features/user/Username';
 function Header() {
   return (
     <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
